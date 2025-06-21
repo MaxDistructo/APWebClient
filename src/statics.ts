cccf5395414120225a02ed7be76f857e0f7aafe7
MaxDistructo <MaxDistructo@users.noreply.github.com>
2025-06-21 09:52:16 -0600

    Initial Commit

---
+++ src/statics.ts
@@ -0,0 +1,16 @@
+// See NetUtils.py in https://github.com/ArchipelagoMW/Archipelago
+// for the original source. We redefine the color codes here for our uses so we match official.
+export enum ColorCodes{
+    BLACK = "#000000",
+    RED = "#EE0000",
+    GREEN = "#00FF7F",
+    YELLOW = "#FAFAD2",
+    BLUE = "#6495ED",
+    MAGENTA = "#EE00EE",
+    CYAN = "#00EEEE",
+    SLATEBLUE = "#6D8BE8",
+    PLUM = "#AF99EF",
+    SALMON = "#FA8072",
+    WHITE = "#FFFFFF",
+    ORANGE = "#FF7700"
+}
